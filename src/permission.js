44612a92fec230c2850784aacd3454a10b59b65d
pxy <13315155984@163.com>
2022-08-19 17:21:52 +0800

    进度条效果

--- src/permission.js
+++ src/permission.js
@@ -2,22 +2,34 @@ import router from '@/router'
 
 import store from '@/store'
 
+// 引入进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 const writeList = ['/login', '/404']
 
 router.beforeEach((to, from, next) => {
+  NProgress.start()
   const token = store.getters.token
   if (token) { // 有token
     // 还要去登录页就直接跳转到首页，去别的页就放行
     if (to.path === '/login') {
       next('/')
+      NProgress.done()
     } else {
       next()
     }
   } else { // 没有token
+    // 看看是否在白名单中，如果在就放行，不在就跳转到登录页
     if (writeList.includes(to.path)) {
       next()
     } else {
       next('/login')
+      NProgress.done()
     }
   }
+})
+
+router.afterEach(_ => {
+  NProgress.done()
 })
