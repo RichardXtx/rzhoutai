3969d6ea54157467ec0d87099b9b988b55ced7de
pxy <13315155984@163.com>
2022-08-19 17:50:34 +0800

    左侧导航基本样式完成

--- src/settings.js
+++ src/settings.js
@@ -7,7 +7,7 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: true,
+  fixedHeader: false,
 
   /**
    * @type {boolean} true | false
