bbc836b6dbb7a60f59cb90facdf14deadf92ed6a
pxy <13315155984@163.com>
2022-08-25 12:26:18 +0800

    loading加载效果

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,7 +4,7 @@
     class="departments-container"
     element-loading-text="拼命加载中"
     element-loading-spinner="el-icon-loading"
-    element-loading-background="rgba(0, 0, 0, 0.8)"
+    element-loading-background="rgba(190, 237, 199, 0.8)"
   >
     <div class="app-container">
       <el-card>
