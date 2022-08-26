accef3cdf964bf726aae398cdd420512dbbbfe9f
pxy <13315155984@163.com>
2022-08-26 19:40:53 +0800

    打开弹框事件

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -1,38 +1,75 @@
 <template>
-  <el-dialog title="新增员工" :visible="showDialog" top="8vh" @close="closeDialog">
+  <el-dialog
+    title="新增员工"
+    :visible="showDialog"
+    top="8vh"
+    @close="closeDialog"
+    @open="openDialog"
+  >
     <!-- 表单 -->
     <el-form ref="addForm" :model="formData" :rules="rules" label-width="120px">
       <el-form-item label="姓名" prop="username">
-        <el-input v-model="formData.username" style="width:50%" placeholder="请输入姓名" />
+        <el-input
+          v-model="formData.username"
+          style="width: 50%"
+          placeholder="请输入姓名"
+        />
       </el-form-item>
       <el-form-item label="手机" prop="mobile">
-        <el-input v-model="formData.mobile" style="width:50%" placeholder="请输入手机号" />
+        <el-input
+          v-model="formData.mobile"
+          style="width: 50%"
+          placeholder="请输入手机号"
+        />
       </el-form-item>
       <el-form-item label="入职时间" prop="timeOfEntry">
-        <el-date-picker v-model="formData.timeOfEntry" style="width:50%" placeholder="请选择入职时间" />
+        <el-date-picker
+          v-model="formData.timeOfEntry"
+          style="width: 50%"
+          placeholder="请选择入职时间"
+        />
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择" />
+        <el-select
+          v-model="formData.formOfEmployment"
+          style="width: 50%"
+          placeholder="请选择"
+        />
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
-        <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号" />
+        <el-input
+          v-model="formData.workNumber"
+          style="width: 50%"
+          placeholder="请输入工号"
+        />
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门" />
+        <el-input
+          v-model="formData.departmentName"
+          style="width: 50%"
+          placeholder="请选择部门"
+        />
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
-        <el-date-picker v-model="formData.correctionTime" style="width:50%" placeholder="请选择转正时间" />
+        <el-date-picker
+          v-model="formData.correctionTime"
+          style="width: 50%"
+          placeholder="请选择转正时间"
+        />
       </el-form-item>
     </el-form>
     <!-- footer插槽 -->
     <template v-slot:footer>
       <el-button class="closeDialog" @click="closeDialog">取消</el-button>
-      <el-button type="primary">确定</el-button>
+      <el-button type="primary" @click="submit">确定</el-button>
     </template>
   </el-dialog>
 </template>
 
 <script>
+import { getDepartmentApi } from '@/api/department'
+
+import { transFromTreeList } from '@/utils/index'
 export default {
   props: {
     showDialog: {
@@ -51,38 +88,92 @@ export default {
         timeOfEntry: '', // 入职时间
         correctionTime: '' // 转正时间
       },
+      list: [],
       rules: {
         username: [
-          { required: true, message: '用户姓名不能为空', trigger: ['blur', 'change'] },
-          { min: 1, max: 4, message: '用户姓名为1-4位', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '用户姓名不能为空',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 1,
+            max: 4,
+            message: '用户姓名为1-4位',
+            trigger: ['blur', 'change']
+          }
         ],
         mobile: [
-          { required: true, message: '手机号不能为空', trigger: ['blur', 'change'] },
-          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '手机号不能为空',
+            trigger: ['blur', 'change']
+          },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '手机号格式不正确',
+            trigger: ['blur', 'change']
+          }
         ],
         formOfEmployment: [
-          { required: true, message: '聘用形式不能为空', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '聘用形式不能为空',
+            trigger: ['blur', 'change']
+          }
         ],
         workNumber: [
-          { required: true, message: '工号不能为空', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '工号不能为空',
+            trigger: ['blur', 'change']
+          }
         ],
         departmentName: [
-          { required: true, message: '部门不能为空', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '部门不能为空',
+            trigger: ['blur', 'change']
+          }
         ],
         timeOfEntry: [
-          { required: true, message: '请选择入职时间', trigger: ['blur', 'change'] }
+          {
+            required: true,
+            message: '请选择入职时间',
+            trigger: ['blur', 'change']
+          }
         ]
       }
     }
   },
   methods: {
     closeDialog() {
-      this.$emit('update:showDialog', false)
+      // 关闭弹框
+      this.$emit('update:showDialog', false) // 通知父组件要关闭
+      this.$refs.addForm.resetFields() // 关闭重置表单的 prop 绑定的值
+    },
+    submit() {
+      // 确定事件
+      this.$refs.addForm.validate((val) => {
+        if (!val) return
+      })
+    },
+    async transFromTree() {
+      // 转换树形图
+      const {
+        data: { depts }
+      } = await getDepartmentApi()
+      //   console.log(res)
+      this.list = transFromTreeList(depts, '')
+      //   console.log(this.list)
+    },
+    openDialog() {
+      // 打开弹框触发的事件
+      this.transFromTree()
     }
   }
 }
 </script>
 
 <style>
-
 </style>
