488dd3703d7ae46239ae0731b894abd384521bf4
pxy <13315155984@163.com>
2022-08-31 09:23:19 +0800

    自定义上传,本地预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <el-upload list-type="picture-card" :on-remove="handleRemove" :limit="limit" :class="{disabled:isDisabled}" action="#" :file-list="fileList" :on-preview="preview">
+    <el-upload
+      :http-request="handleRequest"
+      list-type="picture-card"
+      :on-remove="handleRemove"
+      :limit="limit"
+      :class="{ disabled: isDisabled }"
+      action="#"
+      :file-list="fileList"
+      :on-preview="preview"
+      :on-change="handleChange"
+    >
       <i class="el-icon-plus" />
     </el-upload>
-    <el-dialog width="600px" top="8vh" title="图片预览" :visible.sync="showDialog">
+    <el-dialog
+      width="600px"
+      top="8vh"
+      title="图片预览"
+      :visible.sync="showDialog"
+    >
       <img width="100%" :src="imgUrl" alt="">
     </el-dialog>
   </div>
@@ -23,8 +38,12 @@ export default {
   data() {
     return {
       fileList: [
-        { url: 'https://www.bing.com/th/id/OGC.cfa9aad90c9c26339be49f6da22d6c44?pid=1.7&rurl=https%3a%2f%2fpic1.zhimg.com%2f50%2fv2-cfa9aad90c9c26339be49f6da22d6c44_hd.gif%3fsource%3d1940ef5c&ehk=0beLbuk347uk4nz0FxnUi14WTzgGPuxQxewn4iPWTNE%3d' },
-        { url: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.fhVjrV9uv3w7a1EVDrA7-wHaDW?w=318&h=158&c=7&r=0&o=5&dpr=1.25&pid=1.7' }
+        {
+          url: 'https://www.bing.com/th/id/OGC.cfa9aad90c9c26339be49f6da22d6c44?pid=1.7&rurl=https%3a%2f%2fpic1.zhimg.com%2f50%2fv2-cfa9aad90c9c26339be49f6da22d6c44_hd.gif%3fsource%3d1940ef5c&ehk=0beLbuk347uk4nz0FxnUi14WTzgGPuxQxewn4iPWTNE%3d'
+        },
+        {
+          url: 'https://www.bing.com/th/id/OGC.e4846a5d421a2f83e4ed34fb7a177fcf?pid=1.7&rurl=https%3a%2f%2fbbsfiles.vivo.com.cn%2fvivobbs%2fattachment%2fforum%2f201304%2f04%2f150624khhset6qk89qlqkh.gif&ehk=v8Y1Rn92AeExlVi0y8TkqRTzavhcChLhwil3xTaWtTk%3d'
+        }
       ],
 
       showDialog: false, // 控制显示弹层
@@ -32,9 +51,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      'name'
-    ]),
+    ...mapGetters(['name']),
     isDisabled() {
       return this.fileList.length >= this.limit
     }
@@ -49,25 +66,32 @@ export default {
       // 第一种方法
       // this.fileList = [...fileList]
 
+      // this.fileList = JSON.parse(JSON.stringify(fileList))
+
       // 第二种方法
-      // this.fileList = this.fileList.filter(item => {
-      //   return item.uid !== file.uid
-      // })
-      console.log(file)
+      this.fileList = this.fileList.filter((item) => {
+        return item.uid !== file.uid
+      })
+      // console.log(file)
+      // console.log(fileList)
+    },
+    handleRequest(obj) {
+      // console.log(obj)
+    },
+    handleChange(file, fileList) {
+      this.fileList = [...fileList]
       console.log(fileList)
     }
-
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.disabled{
-  ::v-deep{
-    .el-upload{
+.disabled {
+  ::v-deep {
+    .el-upload {
       display: none;
     }
   }
 }
-
 </style>
